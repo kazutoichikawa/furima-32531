0e51bbda9bdb5d1c2157b738121d96030a99f165
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2020-12-15 18:11:40 +0900

    Userモデルにバリテーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :comments
+  has_many :items
+
+ validates :nickname, presence: true
+ validates :email, presence: true
+ validates :password, presence: true
+ validates :l_name, presence: true
+ validates :f_name, presence: true
+ validates :l_name_r, presence: true
+ validates :f_name_r, presence: true
+ validates :bday_y, presence: true
+ validates :bday_m, presence: true
+ validates :bday_d, presence: true
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
