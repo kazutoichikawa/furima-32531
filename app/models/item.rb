d893ca865124b9352b1db5ad55922dd52f93e519
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2020-12-17 18:18:45 +0900

    出品金額のバリデーション設定
    300 ~ 9999999

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,6 +11,12 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :title
     validates :explain
+    validates :price, numericality: {
+                                    only_integer: true,
+                                    greater_than_or_equal_to: 300,
+                                    less_than_or_equal_to: 9999999
+                                    }
+    
   end
   with_options numericality: { other_than: 1 } do
     validates :category_id
