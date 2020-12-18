a6ffbc9263b64bce2ffea29501f32e99566b1d3e
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2020-12-18 18:21:25 +0900

    destroyアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :destroy]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -18,10 +19,20 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    
+    @item.destroy
+    redirect_to root_path
+  end
+
+
+  private
+  def set_item
     @item = Item.find(params[:id])
   end
 
-  private
   def item_params
     params.require(:item).permit(:image, :title, :explain, :category_id, :condition_id, :s_fee_id, :area_id, :d_f_ship_id, :price).merge(user_id: current_user.id)
   end
