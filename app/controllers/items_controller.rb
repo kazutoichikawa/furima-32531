f5cbef52b1d67959712eb5ba4885a636e1ab77eb
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2020-12-18 20:32:25 +0900

    edit,updateアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :destroy, :edit]
+  before_action :set_item, only: [:show, :destroy, :edit, :update]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -29,8 +29,19 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
+  def update
+    @item.update(item_params)
+    if @item.save
+      redirect_to "/items/#{@item.id}"
+    else
+      render :edit
+    end
+  end
 
   private
   def set_item
