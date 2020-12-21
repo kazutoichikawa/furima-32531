827fbc1cf51a5d3127eeb469f13d30b561a0297f
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2020-12-21 19:23:12 +0900

    テストコードの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     if @order.valid?
+      pay_item
       @order.save
       return redirevt_to root_path
     else
@@ -17,5 +18,14 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order).permit(:price)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_096bd2d072851977f71c04f4"
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
