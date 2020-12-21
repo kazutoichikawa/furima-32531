862bea7af8f3128a6fdc1a960c37451798bcae7c
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2020-12-21 16:43:41 +0900

    トークン生成のテストコード記述

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,13 +5,20 @@ RSpec.describe Order, type: :model do
     @order = FactoryBot.build(:order)
   end
 
-  it "priceがあれば保存できること" do
+  it "priceとtokenがあれば保存できる" do
     expect(@order).to be_valid
   end
 
-  it "priceが空では保存ができないこと" do
-    @order = nil
+  it "priceが空では保存ができない" do
+    @order.price = nil
     @order.valid?
     expect(@order.errors.full_messages).to include("Price can't be blank")
   end
+
+  it "tokenが空では登録できない" do
+    @order.token = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("Token can't be blank")
+  end
+
 end
