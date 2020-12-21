f86bd8b8ccf0692df217b90cfbea3f15a6148cba
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2020-12-21 12:45:48 +0900

    ビューファイルの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,21 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirevt_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:price)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  has_many :items
+
   validates :price, presence: true
-  validates :token, presence: true
 end
