2fcd1465f6e1a95f18673f6205c76e05d6f0c5af
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2020-12-23 19:31:46 +0900

    ビューファイルの編集
    既に売れた商品は編集・削除ができないように設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @order = BuyUser.new
-    if @item.purchase_m.present?
+    if  @item.purchase_m.present?
         redirect_to root_path
     end
   end
